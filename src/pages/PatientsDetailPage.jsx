902c2074f3ceaf67a3ba43233cfab92a9c3cf202
Michael Franke <michael.a.franke@stud.h-da.de>
2024-12-26 18:51:05 +0100

    Entwurf Grid

--- src/pages/PatientsDetailPage.jsx
+++ src/pages/PatientsDetailPage.jsx
@@ -3,14 +3,51 @@ import Sidebar from "../components/Sidebar.jsx";
 import './PatientsDetailPage.css';
 
 
-
 function PatientsDetailPage() {
     return (
-        <div className="patientsDetailPageContainer">
-            <Sidebar name='Selma Lepka' image='/src/assets/selma.jpg'/>
-            <h1>Grid für Layout anlegen - PatientDetailPage</h1>
+        <div className="container">
+            <div className="Sidebar"><Sidebar/></div>
+            <div className="PreviousNote">
+                <h1>Notiz 1</h1>
+                <p>Text Text Text</p>
+            </div>
+            <div className="NewNote">
+                <h1>Neue Notiz</h1>
+                <textarea></textarea>
+                <button>Speichern</button>
+            </div>
+            <div className="PatientMainContent">
+                <div className="PatientName">
+                    <h1>Selma Lepka</h1>
+                </div>
+                <div className="PatientImage">
+                    <img src="/src/assets/selma.jpg" alt="Selma Lepka" className="profile-image"/>
+                </div>
+                <div className="PatientInfos">
+                    <h1>infos, allergien etc. </h1>
+                    <p>Allergies</p>
+                    <p>Birthdate</p>
+                    <p>Current Diagnos</p>
+                    <p>Current Medication</p>
+                </div>
+                <div className="Timeline">
+                    <h1>Timeline</h1>
+                </div>
+            </div>
         </div>
     );
 }
 
 export default PatientsDetailPage;
+
+<div className="container">
+    <div className="Sidebar"></div>
+    <div className="PreviousNote"></div>
+    <div className="NewNote"></div>
+    <div className="PatientMainContent">
+        <div className="PatientInfos"></div>
+        <div className="Timeline"></div>
+        <div className="PatientName"></div>
+        <div className="PatientImage"></div>
+    </div>
+</div>
